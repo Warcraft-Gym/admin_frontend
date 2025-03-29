12b83ee60c0fd945a3ce2480288947d227d46093
monsieur_merlin <nonar.sibudo@gmail.com>
2025-03-29 14:06:46 +0100

    initial commit - frontend App

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # admin_frontend
+
+# introduction
+The whole app is based on this tutorial 
+
+Vue 3 + Pinia - JWT Authentication Tutorial & Example
+Documentation at https://jasonwatmore.com/post/2022/05/26/vue-3-pinia-jwt-authentication-tutorial-example
+
+# setting up the project
+
+1) run a terminal and the command : npm install
+
+2) run the command : npm run dev
+
+3) You should have the adress of your local in the terminal
+
+Have fun 😃
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { storeToRefs } from 'pinia';
+
+import { useAuthStore, useUsersStore } from '@/stores';
+
+const authStore = useAuthStore();
+const { user: authUser } = storeToRefs(authStore);
+
+const usersStore = useUsersStore();
+const { users } = storeToRefs(usersStore);
+
+usersStore.getAll();
+</script>
+
+<template>
+    <div>
+        <h1>Welcome to the GNL APP</h1>
+        <p>Note : include docs / nav etc.</p>
+    </div>
+</template>
