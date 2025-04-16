581fc9dab70eecc447eb4c7aa02df9ce78821f42
Martin Bauer <eashibby@gmx.de>
2025-04-16 21:08:00 +0200

    Dockerize

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,14 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    server: {
+        host: true, // Allow access from outside the container (important for Docker)
+        port: 5003, // Matches the port exposed by your Dockerfile
+        strictPort: true, // Ensures the port is available and does not auto-select another
+        watch: {
+          usePolling: true // Necessary for file changes when using Docker on some systems
+        }
+      },    
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
